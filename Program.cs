ae8f9627870b0bd0cc9037fcd2e7f26406fb8695
BlodyxCZ <kjubercz@gmail.com>
2023-09-11 14:59:59 +0200

    Added Rectangle class

--- Program.cs
+++ Program.cs
@@ -5,3 +5,19 @@ class Program {
         Console.WriteLine("Hello World!");
     }
 }
+
+class Rectangle {
+    public: {
+        float width;
+        float height;
+    }
+
+    public Rectangle(float width, float height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void Draw() {
+        Console.WriteLine("Drawing a rectangle with width {0} and height {1}", width, height);
+    }
+}
